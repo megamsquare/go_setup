a0cd94ae8a782d499ba833206a666c21da607fe7
Michael <msquaremega@gmail.com>
2023-04-17 22:16:46 +0100

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Start Golang")
+	A := []int{2,5,2,7,4}
+	fmt.Println(distAbsDiff(A))
+}
+
+func distAbsDiff(A []int) int {
+	result := make(map[int]bool)
+	for i := 1; i < len(A); i++ {
+		diff := abs(A[i] - A[i - 1])
+		result[diff] = true
+	}
+
+	return len(result)
+}
+
+func abs(x int) int {
+	// fmt.Println(x)
+	if x < 0 {
+		return -x
+	}
+	return x
+}
