91684464cf1c5f5ff65b9efbbd3de6a913180ed7
Michael <msquaremega@gmail.com>
2023-04-18 17:52:36 +0100

    config file in progress

---
+++ config/config.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"bufio"
+	"errors"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/kelseyhightower/envconfig"
+)
+
+var env_prefix = ""
+
+func Load(t interface{}) {
+	Load_with_prefix(env_prefix, t)
+}
+
+func Load_with_prefix(prefix string, t interface{}) {
+	if err := envconfig.Process(prefix, t); err != nil {
+		log.Println("Unable to read: ", err)
+	}
+}
+
+func Set_env(f string) error {
+	file, err := os.Open(f)
+	if err != nil {
+		return nil
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		txt := scanner.Text()
+		if strings.HasPrefix(txt, "#") || strings.TrimSpace(txt) == "" {
+			continue
+		}
+		env := strings.SplitN(txt, "=", 2)
+		if len(env) != 2 {
+			return errors.New("must be key-value pair")
+		}
+		log.Println(env)
+	}
+
+	return nil
+}
