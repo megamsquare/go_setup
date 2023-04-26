f22fd02eac0ec13e213bad5a742adb7f353e8d1a
Michael <msquaremega@gmail.com>
2023-04-26 02:39:48 +0100

    rabbitmq connection done, next is to attach it to main.go

--- pkg/queue/queue.go
+++ pkg/queue/queue.go
@@ -14,7 +14,10 @@ type RabbitMQ struct {
 }
 
 type RabbitMQConfig struct {
-	URI string `config:"AMQP_URI" default:"amqp://guest:guest@localhost:5672/"`
+	Username string `config:"RABBITMQ_USER" default:"rabbitmq"`
+	Password string `config:"RABBITMQ_PASSWORD" default:"rabbitmq"`
+	Host string `config:"RABBITMQ_HOST" default:"rabbitmq"`
+	Port int `config:"RABBITMQ_PORT" default:"5672"`
 }
 
 func Load_config() *RabbitMQConfig {
@@ -24,20 +27,20 @@ func Load_config() *RabbitMQConfig {
 }
 
 func Connect_rabbitMQ(conf *RabbitMQConfig) (*RabbitMQ, error) {
-	conn := fmt.Sprintf(conf.URI)
+	rabbitMQURL := fmt.Sprintf("amqp://%s:%s@%s:%d", conf.Username, conf.Password, conf.Host, conf.Port)
 
-	rbmq, err := amqp.Dial(conn)
+	conn, err := amqp.Dial(rabbitMQURL)
 	if err != nil {
 		return nil, err
 	}
 
-	channel, err := rbmq.Channel()
+	channel, err := conn.Channel()
 	if err != nil {
 		return nil, err
 	}
 
 	rabbitMQ := &RabbitMQ {
-		conn: rbmq,
+		conn: conn,
 		channel: channel,
 	}
 
