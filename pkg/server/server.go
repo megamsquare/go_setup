4123b1647b5f701064ef2b3573b98541b7d03301
Michael <msquaremega@gmail.com>
2023-05-02 18:22:58 +0100

    set load config for server

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,1 +1,21 @@
 package server
+
+import (
+	"time"
+
+	config "github.com/megamsquare/go_setup/pkg/env_config"
+)
+
+type ServerConfig struct {
+	Address string `config:"HTTP_ADDRESS"`
+	Port    int    `config:"HTTP_PORT" default:"3000"`
+	ReadTimeout time.Duration `config:"HTTP_READ_TIMEOUT" default:"10s"`
+	ShutdownTimeout   time.Duration `config:"HTTP_SHUTDOWN_TIMEOUT" default:"10s"`
+}
+
+func Load_config() *ServerConfig {
+	var conf ServerConfig
+	config.Load(&conf)
+	return &conf
+}
+
