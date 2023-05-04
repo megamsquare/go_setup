577d99b383dd41ad4942d75c3e4057919475dec1
Michael <msquaremega@gmail.com>
2023-05-04 20:33:19 +0100

    add docker settigs to makefile

--- Makefile
+++ Makefile
@@ -5,3 +5,11 @@ up:
 .PHONEY: down
 down:
 	docker-compose down
+
+.PHONEY: remove_image
+remove_image:
+	docker rmi $(docker images -a -q)
+
+.PHONEY: remove_val
+remove_val:
+	docker volume prune
--- cmd/main.go
+++ cmd/main.go
@@ -8,14 +8,18 @@ import (
 	database "github.com/megamsquare/go_setup/pkg/db"
 	config "github.com/megamsquare/go_setup/pkg/env_config"
 	"github.com/megamsquare/go_setup/pkg/queue"
+	"github.com/megamsquare/go_setup/pkg/grpc"
+
 )
 
 func main() {
+	// Call enveronment var
 	err := config.Set_env(".env")
 	if err != nil {
 		log.Println("env file load config error: ", err)
 	}
 
+	// Init RabbitMQ
 	rmqConfig := queue.Load_config()
 	rabbitMQ, err := queue.Connect_rabbitMQ(rmqConfig)
 	if err != nil {
@@ -23,6 +27,7 @@ func main() {
 	}
 	defer rabbitMQ.Close()
 
+	// Init DB
 	dbConfig := database.Load_config()
 	db, err := database.Connect_db(dbConfig)
 	if err != nil {
@@ -30,9 +35,12 @@ func main() {
 	}
 	defer db.Close()
 
-	http.HandleFunc("/", handler)
-	log.Println("Starting Server at port :3000")
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	gRPCconfig := grpc.Load_config()
+	grpc.StartGRPCServer(*gRPCconfig)
+
+	// http.HandleFunc("/", handler)
+	// log.Println("Starting Server at port :3000")
+	// log.Fatal(http.ListenAndServe(":3000", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
