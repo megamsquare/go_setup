0619428ea3b5cd8248ec8f68f67ecf15d296b503
Michael <msquaremega@gmail.com>
2023-05-08 21:55:32 +0100

    migration folder and files moved to pkg

--- database/migration/000001_users_table.down.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS "users" (
-    "id" SERIAL PRIMARY KEY,
-    "userName" VARCHAR (64) NOT NULL,
-    "firstName" VARCHAR (64) NOT NULL,
-    "lastName" VARCHAR (64) NOT NULL DEFAULT (NOW()),
-    "created_at" TIMESTAMP NOT NULL DEFAULT (NOW()),
-    "updated_at" TIMESTAMP NOT NULL DEFAULT (NOW())
-);
